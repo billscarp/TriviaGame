beaf4b1d9d51f03034bbfe108113146d5ac5c8d6
billscarp <billscarp@gmail.com>
2017-08-28 20:14:40 -0400

    Q&A

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,50 @@
 $(document.ready(funciton() {
 
-    
-}
+    // I'm going to start by writing Q&A  //
+
+    window.onload = function() {
+    var questions =
+    [
+      {
+        question: "What is jQuery?",
+        choices: ["a chewing gum", "a search engine", "a travel agency", " A JavaScript library"],
+        answer: 4
+      },
+
+      {
+        question: "What symbol is used to access jQuery?",
+        choices: ["$", "&", "*", "The symbol formally known as Prince"],
+        answer: 1
+      
+    {
+        question: "What is Traversing?",
+        choices: ["A gener operation", "Two songs mixed together", "Selects Elements based on other Elements.", "A new Reality show."],
+        answer: 3
+        },
+
+        question: "",
+        choices: ["", "", "", " "],
+        answer: 4
+        },
+
+        question: "",
+        choices: [" ", "", "", " "],
+        answer: 4
+        },
+
+        question: "",
+        choices: ["", "", "", "  "],
+        answer: 2
+        },
+
+        question: "",
+        choices: ["", "", "", " "],
+        answer: 1
+        },
+
+        question: "",
+        choices: ["", "", "", " "],
+        answer: 2
+        },
+  
+];
